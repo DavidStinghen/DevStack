33c872b81bf7f7432a5b61e223434d7688db6120
DavidStinghen <davidstinghen@gmail.com>
2019-11-12 23:30:03 -0300

    feature: create routes for redirectoin to job's methods

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,6 +6,9 @@ import SessionController from "./app/controllers/SessionController";
 import authMiddleware from "./app/middlewares/auth";
 import multerConfig from "./config/multer";
 import FileController from "./app/controllers/FileController";
+import JobController from "./app/controllers/JobController";
+import JobFileController from "./app/controllers/JobFileController";
+import ShareController from "./app/controllers/shareController";
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,5 +21,18 @@ routes.use(authMiddleware);
 routes.put("/users", UserController.update);
 
 routes.post("/files", upload.single("file"), FileController.store);
+
+routes.get("/jobs", JobController.index);
+routes.get("/jobs/:id", JobController.show);
+routes.post("/jobs", JobController.store);
+routes.put("/jobs/:id", JobController.update);
+routes.delete("/jobs/:id", JobController.delete);
+
+routes.post("/jobfiles", JobFileController.store);
+routes.delete("/jobfiles/:id", JobFileController.delete);
+
+routes.get("/shares", ShareController.index);
+routes.post("/shares", ShareController.store);
+routes.delete("/shares/:id", ShareController.delete);
 
 export default routes;
