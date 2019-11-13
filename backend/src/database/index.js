a7e5539fb9d298b9815154723eb22fc5b8d700e5
DavidStinghen <davidstinghen@gmail.com>
2019-11-12 23:30:26 -0300

    chore: add models to loader

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -3,8 +3,11 @@ import Sequelize from "sequelize";
 import dataBaseConfig from "../config/database";
 import User from "../app/models/User";
 import File from "../app/models/File";
+import Job from "../app/models/Job";
+import JobFile from "../app/models/JobFile";
+import Share from "../app/models/Share";
 
-const models = [User, File];
+const models = [User, File, Job, JobFile, Share];
 
 class Database {
   constructor() {
