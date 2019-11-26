e36939c6020eaa3aacd71ccc9dfcf1eae856b5f8
DavidStinghen <davidstinghen@gmail.com>
2019-11-25 23:36:27 -0300

    feature: add signin page

---
+++ frontend/src/pages/SignIn/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { signInRequest } from '../../store/modules/auth/actions';
+import logo from '../../assets/logo.png';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Informe um e-mail válido')
+    .required('Você deve informar seu e-mail'),
+  password: Yup.string().required('Vocẽ deve informar sua senha'),
+});
+
+export default function SignIn() {
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
+
+  return (
+    <>
+      <img className="logo" src={logo} alt="DevStack" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Informe seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Informe sua senha"
+        />
+
+        <button type="submit">{loading ? 'Carregando...' : 'Acessar'}</button>
+        <Link to="register">Criar uma conta</Link>
+      </Form>
+    </>
+  );
+}
