321bfdb1810d9d479d2941034aeaabcd0d727269
DavidStinghen <davidstinghen@gmail.com>
2019-11-25 23:36:47 -0300

    feature: add signup page

---
+++ frontend/src/pages/SignUp/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { signUpRequest } from '../../store/modules/auth/actions';
+import logo from '../../assets/logo.png';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('Você deve informar seu nome'),
+  email: Yup.string()
+    .email('Informe um e-mail válido')
+    .required('Você deve informar seu e-mail'),
+  password: Yup.string()
+    .min(8, 'A senha deve ter no mínimo 8 caracteres')
+    .required('Vocẽ deve informar sua senha'),
+});
+
+export default function SignUp() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+
+  return (
+    <>
+      <img className="logo" src={logo} alt="DevStack" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Informe seu nome completo" />
+        <Input name="email" type="email" placeholder="Informe seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Informe sua senha"
+        />
+
+        <button type="submit">Cadastre-se</button>
+        <Link to="/">Já tenho uma conta</Link>
+      </Form>
+    </>
+  );
+}
